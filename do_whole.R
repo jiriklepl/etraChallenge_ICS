5da7744a0655ce7f0293cdc5e0e1b14eebcb6d33
jiriklepl <jiriklepl@seznam.cz>
2021-01-02 20:28:56 +0100

    added do_whole for raw data

---
+++ do_whole.R
@@ -0,0 +1,17 @@
+library(raster)
+library(imager)
+library(tidyverse)
+library(here)
+library(ez)
+library(saccades)
+require(ggplot2) 
+
+library(papaja)
+
+
+source("solution.R")
+
+first_hyp()
+second_hyp()
+third_hyp()
+third_hyp_two()
